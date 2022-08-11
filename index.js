83ef6ffa1c7d4d156f81c4f44d4d67984bbd475d
raya <parischax@gmail.com>
2022-08-11 09:14:10 +0300

    done assignments

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+
+    }
+return messages;
+}
+
+(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >=0) {
+        countDown--;
+        console.log(countDown);
+    }
+return countDown;
+}
